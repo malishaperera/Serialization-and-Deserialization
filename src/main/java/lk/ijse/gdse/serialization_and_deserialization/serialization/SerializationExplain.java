a06672afb444633c874424379a9131f0a24fef1c
Malisha Perera <140098754+malishaperera@users.noreply.github.com>
2024-07-19 11:47:55 +0530

    add code

--- src/main/java/lk/ijse/gdse/serialization_and_deserialization/serialization/SerializationExplain.java
+++ src/main/java/lk/ijse/gdse/serialization_and_deserialization/serialization/SerializationExplain.java
@@ -3,19 +3,23 @@ package lk.ijse.gdse.serialization_and_deserialization.serialization;
 import java.io.*;
 
 public class SerializationExplain {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Student student = new Student("Malisha", 22,"malisha01@gmail.com","Galle");
+    public static void main(String[] args) {
+        Student student = new Student("Malisha", 22);
 
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(baos);
-        oos.writeObject(student);
-        byte[] serializedData = baos.toByteArray();
+        // Serialization
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("person.ser"))) {
+            oos.writeObject(student);
+            System.out.println("Serialization complete: " + student);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        ByteArrayInputStream basis
-                = new ByteArrayInputStream(serializedData);
-        ObjectInputStream ois = new ObjectInputStream(basis);
-        Student deserializedStudent = (Student) ois.readObject();
-
-        System.out.println("Deserialized Person: " + "Name:" + deserializedStudent.getName()  + ", " +"Age:" + deserializedStudent.getAge() +", " +"Email:" + deserializedStudent.getEmail() + ", " +"City:" + deserializedStudent.getCity());
+        // Deserialization
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("person.ser"))) {
+            Student deserializedPerson = (Student) ois.readObject();
+            System.out.println("Deserialization complete: " + deserializedPerson);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/lk/ijse/gdse/serialization_and_deserialization/serialization/Student.java
+++ src/main/java/lk/ijse/gdse/serialization_and_deserialization/serialization/Student.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Student {
+public class Student implements Serializable {
+        private static final long serialVersionUID = 1L;
         private String name;
         private int age;
-        private String email;
-        private String city;
 }
